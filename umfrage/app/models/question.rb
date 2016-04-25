e79422552adce3d67d339daaccf204ada7d3afef
Alexis Rinaldoni <arinaldoni@me.com>
2016-04-26 00:10:28 +0200

    add Question#answers (fix QuestionsController#show)

--- umfrage/app/models/question.rb
+++ umfrage/app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ActiveRecord::Base
 	has_many :i_answers
 	has_many :tf_answers
 	# validates :title, presence: true, length: {minimum: 5}
+
+	def answers
+		return s_answers + i_answers + tf_answers
+	end
 end
